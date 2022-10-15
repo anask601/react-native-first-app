533c3aba6202ec489e89015489f110f0ac9d5829
anask601 <anask60155@gmail.com>
2022-10-15 07:43:11 +0530

    added start file

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput placeholder="Add Your Goals 💪" />
+        <Button title="ADD"></Button>
+      </View>
+      <View></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
