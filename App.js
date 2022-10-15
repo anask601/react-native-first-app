20abe5246a7a844cd41eb79cce5202fd2a40e5d5
anask601 <anask60155@gmail.com>
2022-10-15 07:54:38 +0530

    added inline styling to textinput and button

--- App.js
+++ App.js
@@ -4,8 +4,23 @@ import { StyleSheet, TextInput, Text, View, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
-        <TextInput placeholder="Add Your Goals 💪" />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Add Your Goals 💪"
+          style={{
+            width: "80%",
+            borderColor: "#333",
+            borderWidth: 1,
+            padding: 3,
+            marginBottom: 1,
+          }}
+        />
         <Button title="ADD"></Button>
       </View>
       <View></View>
